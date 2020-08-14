a834267c9366a0e22797a435d396351a70c9ec61
unknown <daotuananh201295.com>
2020-08-14 20:35:39 +0700

    [Feature] Create Models

---
+++ app/Models/Student.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Scout\Searchable;
+
+class Student extends Model
+{
+    use SoftDeletes;
+    use Searchable;
+
+    protected $table = 'students';
+    protected $fillable = [
+        'name',
+        'dob',
+        'address',
+        'phone',
+        'classrooms_id',
+    ];
+
+    
+    public function class()
+    {
+        return $this->belongsTo(Classroom::class);
+    }
+
+    public function testScores()
+    {
+        return $this->hasMany(TestScore::class);
+    }
+
+    public function toSearchableArray()
+    {
+        $array = $this->only('name');
+        
+        return $array;
+    }
+}
